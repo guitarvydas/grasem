47495ce6f607adf4b6141021705aa113f2307e2e
Paul Tarvydas <guitarvydas@gmail.com>
2021-04-17 15:12:01 -0400

    removed redundant echos

--- run.bash
+++ run.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo '*** GRASEM ***' >/dev/fd/2
 set -e
 
 # grammar+semantics all in one file
@@ -16,10 +15,7 @@ mv _aa _.ohm
 mv _ab _.glue
 ## now we have _.ohm and _.glue as separate files
 ## create JS file for semantics
-echo '  *** GRASEM creating semantics ***' >/dev/fd/2
 node ../glue/gen-glue.js <_.glue >_semantics.js
 ## and combine everything into one JS file (with support JS) and run the JS program
 cat ../grasem/parser1.js _.ohm ../grasem/parser2.js _semantics.js ../grasem/parser3.js foreign.js >_program.js
-echo '  *** GRASEM running generated parser on input ***' >/dev/fd/2
 node _program.js
-echo '  *** GRASEM DONE ***' >/dev/fd/2
